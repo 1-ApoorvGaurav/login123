2fec640c5ea1626758fee19abc4c04a15219d11b
1-ApoorvGaurav <67899065+1-ApoorvGaurav@users.noreply.github.com>
2020-08-25 23:09:13 -0700

    login
    login er

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_SERVER','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','login');
+//Making connection 
+$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+//Check connection
+if($conn == false){
+    dir('Error: Cannot connect');
+}
+
+?>
